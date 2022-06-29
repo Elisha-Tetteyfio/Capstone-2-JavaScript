760ff4f17566b51f7f5c32e73575c2ba666889ad
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-06-29 10:01:50 +0000

    Add list overall view

--- src/index.js
+++ src/index.js
@@ -4,5 +4,18 @@ import './main.scss';
 import Logo from './assets/cat-logo-removebg-preview.png';
 
 const homeLink = document.querySelector('#homeLink');
-const logo = `<img src="${Logo}" alt="o"></img>`;
+const logo = `<img src="${Logo}" alt="o" id="logo"></img>`;
 homeLink.innerHTML += logo;
+
+
+const items = document.querySelector('#items');
+let item1 = `
+    <li>
+        <img src="#" alt="img1">
+        <div> Name 1 </div>
+        <div>
+            <i></i>
+            <div>5 likes</div>
+        </div>
+        <button type="button">Comments</button>
+    </li>`
